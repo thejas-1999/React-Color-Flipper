a8c5a07e24f8cf0210b5bd44c8201d3832799ec9
thejas-1999 <thejask074@gmail.com>
2023-12-16 13:15:08 +0530

    react project for color flip

---
+++ src/Input.js
@@ -0,0 +1,29 @@
+import colorNames from "colornames";
+const Input = ({
+  colorValue,
+  setColorValue,
+  setHexValue,
+  isDarkText,
+  setIsDarkText,
+}) => {
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <label htmlFor="">Add Color Name</label>
+      <input
+        autoFocus
+        type="text"
+        placeholder="Add Color Name..."
+        required
+        value={colorValue}
+        onChange={(e) => {
+          setColorValue(e.target.value);
+          setHexValue(colorNames(e.target.value));
+        }}
+      />
+      <button type="button" onClick={() => setIsDarkText(!isDarkText)}>
+        Toggle Text Color
+      </button>
+    </form>
+  );
+};
+export default Input;
